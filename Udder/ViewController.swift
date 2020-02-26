2c572917d7e64b9d73db0d402ab93664791d8ba4
Alyssa <yangmaster@yangs-Mac-mini.(none)>
2020-02-26 13:29:20 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by yang zhou on 2/26/20.
+//  Copyright © 2020 Alyssa Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
